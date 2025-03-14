145231fa7cbfdfa744669061f9a4368d625573da
Roger Martin-pero Otin <martinimpero@gmail.com>
2025-03-14 16:59:42 +0100

    First commit: Basic 3D python graphics engine

---
+++ shader_program.py
@@ -0,0 +1,30 @@
+class ShaderProgram:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.programs = {}
+        self.programs['default'] = self.get_program('default')
+
+    def get_program(self, shader_program_name):
+        """
+        Load and compile vertex and fragment shaders from files.
+
+        :param shader_name: The name of the shader files (without extension).
+        :return: The compiled shader program.
+        """
+        # Load the vertex shader source code from file
+        with open(f'shaders/{shader_program_name}.vert') as file:
+            vertex_shader = file.read()
+
+        # Load the fragment shader source code from file
+        with open(f'shaders/{shader_program_name}.frag') as file:
+            fragment_shader = file.read()
+
+        # Compile the shaders and create the shader program
+        program = self.ctx.program(
+            vertex_shader=vertex_shader,
+            fragment_shader=fragment_shader
+        )
+        return program
+
+    def destroy(self):
+        [program.release() for program in self.programs.values()]
---
+++ vbo.py
@@ -0,0 +1,94 @@
+import numpy as np
+import moderngl as mgl
+import pywavefront
+class VBO:
+    def __init__(self, ctx):
+        self.vbos = {}
+        self.vbos['cube'] = CubeVBO(ctx)
+        self.vbos['cat'] = CustomVBO(ctx, objectpath='objects/cat/20430_Cat_v1_NEW.obj')
+
+    def destroy(self):
+        [vbo.destroy() for vbo in self.vbos.values()]
+class BaseVBO:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.vbo = self.get_vbo()
+        self.format: str = None
+        self.attrib: list = None
+
+    def get_vertex_data(self): ...
+
+    def get_vbo(self):
+        vertex_data = self.get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        return vbo
+
+    def destroy(self):
+        self.vbo.release()
+
+class CubeVBO(BaseVBO):
+    def __init__(self, ctx):
+        super().__init__(ctx)
+        self.format = '2f 3f 3f'
+        self.attribs = ['in_texcoord_0', 'in_normal', 'in_position']
+
+    @staticmethod
+    def get_data(vertices, indices):
+        data = [vertices[ind] for triangle in indices for ind in triangle]
+        return np.array(data, dtype='f4')
+
+    def get_vertex_data(self):
+        """
+        Define the vertex positions for a triangle in 3D space.
+
+        :return: NumPy array containing the vertex coordinates as float32.
+        """
+        # Define the triangle's vertex positions (x, y, z)
+        vertices = [(-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1),
+                    (-1, 1, -1), (-1, -1, -1), (1, -1, -1), (1, 1, -1)]
+
+        indices = [(0, 2, 3), (0, 1, 2),
+                   (1, 7, 2), (1, 6, 7),
+                   (6, 5, 4), (4, 7, 6),
+                   (3, 4, 5), (3, 5, 0),
+                   (3, 7, 4), (3, 2, 7),
+                   (0, 6, 1), (0, 5, 6)]
+
+        # Convert list to a NumPy array with 32-bit float precision
+        vertex_data = self.get_data(vertices, indices)
+
+        tex_coord = [(0, 0), (1, 0), (1, 1), (0, 1)]
+        tex_coord_indices = [(0, 2, 3), (0, 1, 2),
+                             (0, 2, 3), (0, 1, 2),
+                             (0, 1, 2), (2, 3, 0),
+                             (2, 3, 0), (2, 0, 1),
+                             (0, 2, 3), (0, 1, 2),
+                             (3, 1, 2), (3, 0, 1)]
+        tex_coord_data = self.get_data(tex_coord, tex_coord_indices)
+
+        normals = [(0, 0, 1) * 6,
+                   (1, 0, 0) * 6,
+                   (0, 0, -1) * 6,
+                   (-1, 0, 0) * 6,
+                   (0, 1, 0) * 6,
+                   (0, -1, 0) * 6]
+
+        normals = np.array(normals, dtype='f4').reshape(36, 3)
+        vertex_data = np.hstack([normals, vertex_data])
+        vertex_data = np.hstack([tex_coord_data, vertex_data])
+
+        return vertex_data
+
+class CustomVBO(BaseVBO):
+    def __init__(self, app, objectpath):
+        self.objectPath = objectpath
+        super().__init__(app)
+        self.format = '2f 3f 3f'
+        self.attribs = ['in_texcoord_0', 'in_normal', 'in_position']
+
+    def get_vertex_data(self):
+        objs = pywavefront.Wavefront(self.objectPath, cache=True, parse=True)
+        obj = objs.materials.popitem()[1]
+        vertex_data = obj.vertices
+        vertex_data = np.array(vertex_data, dtype='f4')
+        return vertex_data
