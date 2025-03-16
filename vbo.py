2812dd2b3adc6c59edb6a43a2fe2e81a1e2858bd
Roger Martin-pero Otin <martinimpero@gmail.com>
2025-03-16 21:01:25 +0100

    MP1: Adding skybox environment

--- vbo.py
+++ vbo.py
@@ -6,6 +6,7 @@ class VBO:
         self.vbos = {}
         self.vbos['cube'] = CubeVBO(ctx)
         self.vbos['cat'] = CustomVBO(ctx, objectpath='objects/cat/20430_Cat_v1_NEW.obj')
+        self.vbos['advanced_skybox'] = AdvancedSkyboxVBO(ctx)
 
     def destroy(self):
         [vbo.destroy() for vbo in self.vbos.values()]
@@ -78,7 +79,6 @@ class CubeVBO(BaseVBO):
         vertex_data = np.hstack([tex_coord_data, vertex_data])
 
         return vertex_data
-
 class CustomVBO(BaseVBO):
     def __init__(self, app, objectpath):
         self.objectPath = objectpath
@@ -91,4 +91,49 @@ class CustomVBO(BaseVBO):
         obj = objs.materials.popitem()[1]
         vertex_data = obj.vertices
         vertex_data = np.array(vertex_data, dtype='f4')
+        return vertex_data
+class SkyboxVBO(BaseVBO):
+    def __init__(self, ctx):
+        super().__init__(ctx)
+        self.format = '3f'
+        self.attribs = ['in_position']
+
+    @staticmethod
+    def get_data(vertices, indices):
+        data = [vertices[ind] for triangle in indices for ind in triangle]
+        return np.array(data, dtype='f4')
+
+    def get_vertex_data(self):
+        """
+        Define the vertex positions for a triangle in 3D space.
+
+        :return: NumPy array containing the vertex coordinates as float32.
+        """
+        # Define the triangle's vertex positions (x, y, z)
+        vertices = [(-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1),
+                    (-1, 1, -1), (-1, -1, -1), (1, -1, -1), (1, 1, -1)]
+
+        indices = [(0, 2, 3), (0, 1, 2),
+                   (1, 7, 2), (1, 6, 7),
+                   (6, 5, 4), (4, 7, 6),
+                   (3, 4, 5), (3, 5, 0),
+                   (3, 7, 4), (3, 2, 7),
+                   (0, 6, 1), (0, 5, 6)]
+
+        # Convert list to a NumPy array with 32-bit float precision
+        vertex_data = self.get_data(vertices, indices)
+        vertex_data = np.flip(vertex_data, 1).copy(order='C')
+        return vertex_data
+class AdvancedSkyboxVBO(BaseVBO):
+    def __init__(self, ctx):
+        super().__init__(ctx)
+        self.format = '3f'
+        self.attribs = ['in_position']
+
+    def get_vertex_data(self):
+        # in clip space
+        z = 0.9999
+        vertices = [(-1,-1,z),(1,1,z),(-1,1,z),
+                    (-1,-1,z),(1,-1,z),(1,1,z)]
+        vertex_data = np.array(vertices, dtype='f4')
         return vertex_data
